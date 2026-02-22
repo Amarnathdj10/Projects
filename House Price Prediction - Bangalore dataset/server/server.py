efabcb1a038a27570d6dacbc230af031dee735a6
Amarnathdj10 <amarnathdj56@gmail.com>
2026-02-22 10:58:43 +0530

    added cors

--- House Price Prediction - Bangalore dataset/server/server.py
+++ House Price Prediction - Bangalore dataset/server/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
